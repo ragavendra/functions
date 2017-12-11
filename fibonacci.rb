3456603cfc573c8e21732c3a2b98091256062e65
ragavendra <ragavendra.bn@gmail.com>
2017-12-11 12:27:44 -0800

    added help usage

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,9 +1,36 @@
+#!/usr/bin/env ruby
+
 #Function to return the fibonacci series
+USAGE = <<ENDUSAGE
+Usage:
+	 #fibonacci.rb [-h] [-v] [create [-s shell] [-f]] directory [-w writer] [-o output_file] [-n] [-l log_file]
+	 fibonacci.rb number 
+ENDUSAGE
 
-limit = 100
+HELP = <<ENDHELP
+   -h, --help       Show this help.
+ENDHELP
 
-fib = [0, 1]
-
-limit.times.each do |time|
-  fib.push( fib.last + fib[-2])
+ARGS = { :shell=>'default', :writer=>'chm' } # Setting default values
+if ARGS[:help]
+	puts USAGE
+	puts HELP if ARGS[:help]
+	exit
 end
+
+def fibonacci limit
+
+	#initial array
+	fib = [0, 1]
+
+	limit.times.each do |time|
+
+		#add last and 2nd last element to the array
+		fib.push( fib.last + fib[-2])
+	
+	end
+
+	fib
+end
+	
+puts fibonacci(ARGV[0].to_i).inspect
